993fcc14056c2370e2ad34747389ba2452fec47a
AK91007 <adithyakulgod@outlook.com>
2021-11-25 20:21:56 +0530

    This is my first git commit

---
+++ odoov13_1/__manifest__.py
@@ -0,0 +1,14 @@
+
+
+{
+    'name': 'Odoov13_1a',
+    'version': '13.0.1',
+    'category': 'Inventory, Logistic, Storage',
+    'license': 'AGPL-3',
+    'author': 'Adithya',
+    'company': 'Adithya Group of Institutions',
+    'website': '',
+    'depends': ['stock'],
+    'data': ['views/product.xml'],
+    'installable': True,
+}
