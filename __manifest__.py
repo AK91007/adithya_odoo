341c1064bd524e50615237ed6fe602a4d158e162
AK91007 <adithyakulgod@outlook.com>
2021-11-25 20:28:55 +0530

    Second Commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+	'name' : 'School Management App',
+	'version': '1.1',
+	'Category' : 'Contact Management',
+	'author': 'Adithya',
+    'company': 'Adithya Group of Institutions',
+    'website': '',
+    'depends': ['contacts'],
+    'data': ['views/product.xml'],
+    'installable': True,
+}
+
+
+}
