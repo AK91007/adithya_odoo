d11d6d745576b9f39b2efc11ff2ad3d36aab1778
AK91007 <adithyakulgod@outlook.com>
2021-12-20 00:47:55 +0530

    Added Relational Fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,26 @@
 from odoo import models, fields
 
+class EstatePropertyType(models.Model):
+	_name = 'estate.property.type'
+	_description = 'Estate Property Type'
+
+	name = fields.Char()
+class EstatePropertyTag(models.Model):
+	_name = 'estate.property.tag'
+	_description = 'Estate Property Tag'
+
+	name = fields.Char()
+
+class EstatePropertyOffer(models.Model):
+	_name = 'estate.property.offer'
+	_description = 'Estate Property Offer'
+	_rec_name = 'property_id'
+	property_id = fields.Many2one('estate.property')
+	offer_partner_id = fields.Many2one('res.partner')
+	offer_status = fields.Selection([('accepted','Accepted'),('refused','Refused')])
+	offer_price = fields.Float()
+
+	
 class EstateProperty(models.Model):
 	_name = 'estate.property'
 	_description = 'Test Model'
@@ -19,6 +40,13 @@ class EstateProperty(models.Model):
 	garden_orientation = fields.Selection([('north','North'),('south','South'),('west','West'),('east','East')])
 	active = fields.Boolean()
 	image = fields.Image()
+	property_type_id = fields.Many2one('estate.property.type', string = 'Property Type')
+	salesman_id=fields.Many2one('res.users')
+	buyer_id=fields.Many2one('res.partner')
+	property_tag_id=fields.Many2many('estate.property.tag', string = 'Property Tag')
+	property_offer_id = fields.One2many('estate.property.offer', 'property_id')
 
-	
+
+
+
 
