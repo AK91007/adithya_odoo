41733f4083f03a0fb93e6abd9a0b1860b41d6db0
AK91007 <adithyakulgod@outlook.com>
2022-01-07 00:12:56 +0530

    Updated Estate Account Module

--- estate_account/models/model.py
+++ estate_account/models/model.py
@@ -6,3 +6,16 @@ class EstateProperty(models.Model):
 
 	def action_sold(self):
 		print("\n \n Sold button clicked")
+
+		for record in self:
+			vals={}
+			journal=self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+			vals['partner_id'] = record.buyer_id.id
+			vals['move_type'] = 'out_invoice'
+			vals['journal_id'] = journal.id
+			vals['invoice_line_ids'] = [(0,0,{'name':record.name,'quantity':1,'price_unit':record.selling_price}),
+										(0,0,{'name':'Extra Fees','quantity':1,'price_unit':0.06*record.selling_price}),
+										(0,0,{'name':'Administrative Fees','quantity':1,'price_unit':1000})
+										]
+			
+			self.env['account.move'].create(vals)
