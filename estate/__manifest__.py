f11670f4009bf1a2c0eab107ed5a5b11965fc0e1
AK91007 <adithyakulgod@outlook.com>
2022-01-15 23:20:56 +0530

    Completed till basic reporting

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,7 +9,9 @@
 		'security/ir.model.access.csv',
 		'views/estate_menus.xml',
 		'wizard/create_sold_invoice_view.xml',
-		'views/estate_property_views.xml'
+		'views/estate_property_views.xml',
+		'report/property_report.xml',
+		'report/property_detail.xml'
 	]
 	
 
